2d5cae9faadf9ed2ad6d21e3c0d7d01d2b6c5264
GoyaChen <46504384+GoyaChen@users.noreply.github.com>
2020-04-29 00:46:50 -0700

    final

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int MaxPlayerCount = 3;
+    public int CurPlayerCount = 3;
+    [SerializeField] private Player player = null;
+    [SerializeField] private MeshRenderer[] redCubeMeshRenders = null;
+
+    private void Awake()
+    {
+        CurPlayerCount = MaxPlayerCount;
+        player.OnChangeRed += OnChangeRed;
+        player.OnPlayerDead += OnPlayerDead;
+        
+    }
+
+ 
+    private void Start()
+    {
+        player.ResetPlayer();
+        CurPlayerCount -= 1;
+    }
+
+    private void OnChangeRed()
+    {
+        if (CurPlayerCount <= 0)
+        {
+            UIManager.Instance.ShowGameOver(CheckGameSuccess());
+        }
+    }
+
+    private bool CheckGameSuccess()
+    {
+        for (int i = 0; i < redCubeMeshRenders.Length; i++)
+        {
+            if (redCubeMeshRenders[i].material.color == Color.red)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void OnPlayerDead()
+    {
+        if (CurPlayerCount > 0)
+        {
+            Invoke("DelayResetPlayer", 3.0f);
+        }
+        else
+        {
+            UIManager.Instance.ShowGameOver(false);
+        }
+     
+    }
+
+    private void DelayResetPlayer()
+    {
+        CurPlayerCount -= 1;
+        player.ResetPlayer();
+    }
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance = null;
+    public static UIManager Instance
+    {
+        get { if (instance == null) instance = FindObjectOfType<UIManager>(); return instance; }
+    }
+
+    [SerializeField] private GameObject gameOverUI = null;//游戏结束UI
+    [SerializeField] private Text gameOverText = null;
+    [SerializeField] private Slider heathBar = null;
+    [SerializeField] private Player player = null;
+    [SerializeField] private Text playerCountText = null;
+    [SerializeField] private GameManager gameManager = null;
+
+
+    private void Awake()
+    {
+        gameOverUI.SetActive(false);
+        heathBar.maxValue = player.MaxHealth;
+
+    }
+
+
+    private void Update()
+    {
+        heathBar.value = player.CurrentHealth;
+        playerCountText.text = gameManager.CurPlayerCount.ToString();
+    }
+
+    public void ShowGameOver(bool success)
+    {
+        gameOverText.text = success ? "游戏胜利！" : "游戏失败!";
+        gameOverUI.SetActive(true);
+    }
+}
